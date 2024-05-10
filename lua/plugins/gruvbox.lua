97aa129b99bb4a3ab73aae36372c6509cf855bdf
Sergey <108259528+Maclovi@users.noreply.github.com>
2024-05-10 14:24:02 +0300

    Add files via upload

---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,11 @@
+return {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = true,
+  opts = {
+    transparent_mode = true,
+    overrides = {
+      StatusLine = { bg = "#242424", fg = "#f2e5bc" },
+    },
+  },
+}
